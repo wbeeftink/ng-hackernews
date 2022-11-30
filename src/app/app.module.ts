aab60dd6be2f7bcab143667c8e2a1df9aae4c296
Wouter Beeftink <wouter.beeftink@gmail.com>
2022-11-30 23:11:59 +0100

    @angular/material migration - migration-v15
    Updates the Angular Material to v15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,8 @@ import { SentryErrorHandler } from "./sentry";
 
 // Angular Material
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatButtonModule } from "@angular/material/button";
-import { MatCardModule } from "@angular/material/card";
+import { MatLegacyButtonModule as MatButtonModule } from "@angular/material/legacy-button";
+import { MatLegacyCardModule as MatCardModule } from "@angular/material/legacy-card";
 import { MatIconModule } from "@angular/material/icon";
 
 // Container components
