5d6070a7dae4adf6e4d3628bce210eb550e09bb6
Wouter Beeftink <wouter.beeftink@gmail.com>
2023-01-12 17:51:13 +0100

    Update dependencies

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Don't forget to make the script executable (run `chmod +x update.sh')
+
 # Update Angular (see `ng update --help`)
 ng update @angular/cli @angular/core --create-commits --force
 ng update @angular/material @angular/cdk --create-commits --force
