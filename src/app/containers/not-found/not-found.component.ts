a13b5e04f700c7621663a260333281e2505284bd
Wouter Beeftink <wouter.beeftink@gmail.com>
2023-03-05 17:30:33 +0100

    Add stylelint and fix linting errors

--- src/app/containers/not-found/not-found.component.ts
+++ src/app/containers/not-found/not-found.component.ts
@@ -6,7 +6,6 @@ import { Config } from "../../config";
 @Component({
   selector: "app-not-found",
   templateUrl: "./not-found.component.html",
-  styleUrls: ["./not-found.component.scss"],
 })
 export class NotFoundComponent implements OnInit {
   constructor(private titleService: Title) {}
