58afc13cc5d9990cd377af66042cdbe8583b694d
Wouter Beeftink <wouter.beeftink@gmail.com>
2023-12-24 12:10:26 +0100

    Add dark mode (work in progress)

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,12 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { NgFor } from "@angular/common";
 import { RouterLink, RouterLinkActive } from "@angular/router";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
 
 import { NavItem } from "../../interfaces/nav-item";
+import { Theme, ThemeService } from "src/app/services/theme.service";
 
 @Component({
   selector: "app-header",
@@ -14,6 +16,7 @@ import { NavItem } from "../../interfaces/nav-item";
   standalone: true,
   imports: [
     MatButtonModule,
+    MatIconModule,
     MatToolbarModule,
     NgFor,
     RouterLink,
@@ -28,4 +31,16 @@ export class HeaderComponent {
     { title: "Ask", link: "ask" },
     { title: "Jobs", link: "jobs" },
   ];
+
+  private readonly theme = inject(ThemeService);
+  readonly currentTheme = this.theme.getTheme();
+  readonly Theme = Theme;
+
+  enableDarkMode(): void {
+    this.theme.setTheme(Theme.Dark);
+  }
+
+  enableLightMode(): void {
+    this.theme.setTheme(Theme.Light);
+  }
 }
